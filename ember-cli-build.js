7d3fad818893e84552047417f0ec2882c672a970
Steve Calvert <scalvert@linkedin.com>
2020-09-15 15:26:06 -0700

    Adding example of broken imports

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
@@ -20,5 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  const { Webpack } = require("@embroider/webpack");
+  return require("@embroider/compat").compatBuild(app, Webpack);
 };
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,10 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { setupGlobalA11yHooks } from 'ember-a11y-testing/test-support';
 
 setApplication(Application.create(config.APP));
+
+setupGlobalA11yHooks(() => true);
 
 start();
